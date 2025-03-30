797107e1caa8cba23e48215f9f1bf2164b69bcfe
Andrzej Mikulec <andrzejmikulec@MacBook-Pro-Andrzej.local>
2025-03-30 16:23:25 +0200

    adds mat input to screen two

--- src/app/screen-one/screen-one.module.ts
+++ src/app/screen-one/screen-one.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ScreenOneComponent } from './screen-one.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
   { path: '', component: ScreenOneComponent }
@@ -11,6 +13,8 @@ const routes: Routes = [
   declarations: [ScreenOneComponent],
   imports: [
     CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/app/screen-two/screen-two.module.ts
+++ src/app/screen-two/screen-two.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ScreenTwoComponent } from './screen-two.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
   { path: '', component: ScreenTwoComponent }
@@ -11,6 +13,8 @@ const routes: Routes = [
   declarations: [ScreenTwoComponent],
   imports: [
     CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forChild(routes)
   ]
 })
