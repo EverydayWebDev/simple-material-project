773ffd34c86e34ecc24b713572e20d4228e0646a
Andrzej Mikulec <andrzej.mikulec@primetals.com>
2025-04-01 15:04:26 +0200

    angular material v19

--- src/app/screen-two/screen-two.component.ts
+++ src/app/screen-two/screen-two.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-screen-two',
   templateUrl: './screen-two.component.html',
-  styleUrls: ['./screen-two.component.scss']
+  styleUrls: ['./screen-two.component.scss'],
+  standalone: false
 })
 export class ScreenTwoComponent {
   // Add any logic or properties here if needed
