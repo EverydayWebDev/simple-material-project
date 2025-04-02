7e3d42b51db80f4f225057ac1c95e5235bdc0efa
Andrzej Mikulec <andrzej.mikulec@primetals.com>
2025-04-02 10:24:35 +0200

    simplify mat-tab issue part 2

--- src/app/screen-two/screen-two.component.ts
+++ src/app/screen-two/screen-two.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-screen-two',
@@ -8,23 +7,9 @@ import { BehaviorSubject } from 'rxjs';
   standalone: false
 })
 export class ScreenTwoComponent implements OnDestroy {
-  items$ = new BehaviorSubject<any>([]);
-  intervalId: any;
-
-  actualPassNo: any = undefined;
   // Add any logic or properties here if needed
-  constructor(){
-    this.intervalId = setTimeout(()=>{
-      const arr = [];
-      for(let i of [1,2,3,4]){
-        arr.push(Math.random()*100);
-      }
-      this.items$.next([{id:arr[0]}, {id:arr[1]}, {id:arr[2]}, {id:arr[3]}]  );
-    }, 2000)
-  }
+  constructor(){}
 
   ngOnDestroy(): void {
-    clearTimeout(this.intervalId);
-    this.intervalId = null;
   }
 }
